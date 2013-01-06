f196810336cd0de0a3a65fcf60ffa183613b2197
Mihail Chilyashev <m@chilyashev.com>
2013-01-06 20:02:47 +0200

    Removed unnecessary stdin stuff

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright (C) 2012 Mihail Chilyashev
+ * Copyright (C) 2013 Mihail Chilyashev
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
